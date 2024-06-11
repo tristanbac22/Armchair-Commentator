e36a8d127562520ae76834088bfcd29eec03f178
tristanbac22 <33737724+tristanbac22@users.noreply.github.com>
2024-06-11 16:28:18 -0500

    Combine message components to print result message.

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // Array of comments made watching a match
 const armchairComments = [
-    'That\"s a soft call', 'Keep it up',
+    'That\'s a soft call.', 'Keep it up.',
     'I could do that in my sleep.', 'I could do better than that.',  
-    'That\"s what I want to see.', 'Book an appointment to the optometrist.',
-    'Bet he misses', 'Don\"t blow it.', 'Lucky bounce.', 'Pack it up.',
-    'They threw in the towel.', 'They are phoning it in.', 'That looks like it\"d hurt', 
-    'They are earning their salary.', 'That\"s a good start.', 'They still got time.'
+    'That\'s what I want to see.', 'Book an appointment to the optometrist.',
+    'Bet he misses.', 'Don\'t blow it.', 'Lucky bounce.', 'Pack it up.',
+    'They threw in the towel.', 'They are phoning it in.', 'That looks like it\'d hurt', 
+    'They are earning their salary.', 'That\'s a good start.', 'They still got time.'
 ];
 
 const armchairExclaims = [ 'He is wide open!!!',
@@ -33,5 +33,34 @@ const randIndex = (inArr) => {
     return inArr[index];
 }
 
+/*console.log(randIndex(armchairComments));
+console.log(randIndex(armchairExclaims));
+console.log(randIndex(armchairRhetoricals));*/
+
+// array that will be randomly queried to determine the message structure
+const msgStructure = ['comment', 'exclaim', 'rhetorical'];
+
+//function that sets up the structure and prints message
+const formCommentary = () => {
+    let resultCommentary = '';
+    for(let i = 0; i < 3; i++){
+        let msgType = randIndex(msgStructure);
+        switch (msgType){
+            case 'comment':
+                resultCommentary += (randIndex(armchairComments) + ' ');
+                break;
+            case 'exclaim':
+                resultCommentary += (randIndex(armchairExclaims) + ' ');
+                break;
+            case 'rhetorical':
+                resultCommentary += (randIndex(armchairRhetoricals) + ' ');
+                break;
+        };
+        //console.log(resultCommentary);
+    }
+    return resultCommentary;
+}
+
+console.log(formCommentary());
 
 
